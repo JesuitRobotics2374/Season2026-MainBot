72b2f4f84865b442ce862146fad38951b2d8bde5
Ethan Paes <jhiramat28@jesuitmail.org>
2026-01-24 13:55:16 -0800

    Core

---
+++ src/main/java/frc/robot/Core.java
@@ -0,0 +1,38 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+/** Add your docs here. */
+public class Core {
+    public double maxSpeed;
+    public double maxSpeedTurbo;
+    public boolean isTurbo = false;
+    
+    // public final CommandSwerveDrivetrain drivetrain; 
+    // public final IntakeSubsystem m_IntakeSubsystem = new IntakeSubsystem();
+    // public final IndexerSubsystem m_IndexerSubsystem = new IndexerSubsystem();
+    // public final ShooterSubsystem m_ShooterSubsystem = new ShooterSubsystem();
+    // public final VisionSubsystem m_visionSubsystem = new VisionSubsystem();
+
+    public Core() {
+
+    }
+
+    public void registerAutoCommands() {
+            
+    }
+
+    public void configureShuffleBoard() {
+
+    }
+
+    public void configureBindings() {
+
+    }
+
+    public void forwardAlign() {
+
+    }
+}
