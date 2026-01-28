1ebda58193983451a7fef1aba69a204d99ff19b3
Ethan Paes <jhiramat28@jesuitmail.org>
2026-01-27 17:27:11 -0800

    Shooter Progression
    implemented methods and instantcommands for shooter without specifying motor type
    
    -Divit, Joey, Andrew

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -6,36 +6,64 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 
 public class ShooterSubsystem extends SubsystemBase {
-double speed; 
-//private final (motor name) shooterMotor;
-//private final (something) velocityController;
-//private final relativeEncoder encoder; 
+
+  double speed;
+  private double targetRpm = 0.0;
+
+  private static final double MAX_RPM = 6000.0;
+  private static final double RPM_TO_RPS = 1.0 / 60.0;
+  private static final double CURRENT_LIMIT = 40.0; // Amps
+  // private final (motor name) shooterMotor;
+  // private final (something) velocityController;
+  // private final relativeEncoder encoder;
+
   /** Creates a new Shooter. */
   public ShooterSubsystem() {
 
   }
-/** Starts the shooter when ready to shoot
- * @param speed
- */
+
+  private void startShooting(double speed) {
+    // motor.set(speed);
+  }
+
+  private void stopShooting() {
+    // motor.set(0);
+  }
+
+  private void startPurge(double speed) {
+    // motor.set(-speed);
+  }
+
+
+  /**
+   * Starts the shooter when ready to shoot
+   * 
+   * @param speed
+   */
   public Command startShoot(double speed) {
-    return null; 
-    
+    return new InstantCommand(() -> startShooting(speed));
   }
-/** Stops the shooter when needed
- * @param speed
- */
-  public Command stopShoot(double speed) {
-    return null;
+
+  /**
+   * Stops the shooter when needed
+   * 
+   * @param speed
+   */
+  public Command stopShoot() {
+    return new InstantCommand(() -> stopShooting());
   }
-/** Purges the shooter if balls get stuck
- * @param speed
- * @return
- */
+
+  /**
+   * Purges the shooter if balls get stuck
+   * 
+   * @param speed
+   * @return
+   */
   public Command purgeShooter(double speed) {
-    return null;
-    //
+    return new InstantCommand(() -> startPurge(speed));
   }
 
   @Override
