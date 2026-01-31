d6f7109e8500046b4b71cd3eb468e2f2e8342a37
Ethan Paes <ankim29@jesuitmail.org>
2026-01-31 14:19:14 -0800

    shooter srd

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -8,43 +8,59 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 
+import java.lang.annotation.Retention;
+
+import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.InvertedValue;
+
 public class ShooterSubsystem extends SubsystemBase {
 
-  double speed;
+  private TalonFX shooterLeft;
+  private TalonFX shooterRight;
+
+  private double currentSpeed = 0.0;
   private double targetRpm = 0.0;
 
+  // Constants
   private static final double MAX_RPM = 6000.0;
   private static final double RPM_TO_RPS = 1.0 / 60.0;
   private static final double CURRENT_LIMIT = 40.0; // Amps
-  // private final (motor name) shooterMotor;
-  // private final (something) velocityController;
-  // private final relativeEncoder encoder;
 
   /** Creates a new Shooter. */
   public ShooterSubsystem() {
+    this.shooterLeft = new TalonFX(0); // change device ID
+    this.shooterRight = new TalonFX(1); // change device ID
+
+    TalonFXConfiguration leftConfig = new TalonFXConfiguration();
+    TalonFXConfiguration rightConfig = new TalonFXConfiguration();
+
+    leftConfig.CurrentLimits.SupplyCurrentLimitEnable = true;
+    leftConfig.CurrentLimits.SupplyCurrentLimit = CURRENT_LIMIT;
+    leftConfig.MotorOutput.Inverted = InvertedValue.Clockwise_Positive;
+
+    rightConfig.CurrentLimits.SupplyCurrentLimitEnable = true;
+    rightConfig.CurrentLimits.SupplyCurrentLimit = CURRENT_LIMIT;
+    rightConfig.MotorOutput.Inverted = InvertedValue.CounterClockwise_Positive;
+
+    shooterLeft.getConfigurator().apply(leftConfig);
+    shooterRight.getConfigurator().apply(rightConfig);
 
   }
 
-  private void startShooting(double speed) {
-    // motor.set(speed);
+  private void setSpeed(double speed) {
+    shooterLeft.set(speed);
+    shooterRight.set(speed);
   }
-
-  private void stopShooting() {
-    // motor.set(0);
-  }
-
-  private void startPurge(double speed) {
-    // motor.set(-speed);
-  }
-
 
   /**
    * Starts the shooter when ready to shoot
    * 
    * @param speed
    */
-  public Command startShoot(double speed) {
-    return new InstantCommand(() -> startShooting(speed));
+  public Command shoot(double speed) {
+    return new InstantCommand(() -> setSpeed(Math.abs(speed)), this);
   }
 
   /**
@@ -53,17 +69,17 @@ public class ShooterSubsystem extends SubsystemBase {
    * @param speed
    */
   public Command stopShoot() {
-    return new InstantCommand(() -> stopShooting());
+    return new InstantCommand(() -> setSpeed(0), this);
   }
 
   /**
    * Purges the shooter if balls get stuck
    * 
-   * @param speed
+   * @param speed output speed
    * @return
    */
   public Command purgeShooter(double speed) {
-    return new InstantCommand(() -> startPurge(speed));
+    return new InstantCommand(() -> setSpeed(-Math.abs(speed)), this);
   }
 
   @Override
