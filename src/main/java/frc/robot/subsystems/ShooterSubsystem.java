6df6d4617bb11c269a80d72ec7c48eb165b5aba1
Juli <jhiramat28@jesuitmail.org>
2026-02-24 17:02:40 -0800

    hood code
    not its own subsystem just part of shooter
    core changes might be necessary but idk how to do them

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -1,4 +1,3 @@
-
 package frc.robot.subsystems;
 
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
@@ -17,11 +16,12 @@ public class ShooterSubsystem extends SubsystemBase {
 
     private final TalonFX control;
     private final TalonFX follower;
+    private final TalonFX hood;
     
     // Request object to avoid allocation in loops
     private final VelocityVoltage velocityRequest = new VelocityVoltage(0).withSlot(0);
 
-    private double targetRpm = 0.0;
+    private double targetRpm = 0.0; 
     
     // Constants
     private static final double MAX_RPM = 6000.0; 
@@ -32,8 +32,10 @@ public class ShooterSubsystem extends SubsystemBase {
 
         control = new TalonFX(11);
         follower = new TalonFX(12);
+        hood = new TalonFX(13); // placeholder 
 
         TalonFXConfiguration controlCfg = new TalonFXConfiguration();
+        TalonFXConfiguration hoodCfg = new TalonFXConfiguration();
         controlCfg.MotorOutput.NeutralMode = NeutralModeValue.Brake;
         controlCfg.MotorOutput.Inverted = InvertedValue.Clockwise_Positive;
         
@@ -46,7 +48,13 @@ public class ShooterSubsystem extends SubsystemBase {
         controlCfg.Slot0.kD = 0.0001;
         controlCfg.Slot0.kV = 0.12; // ~12V
 
+        // for hood config i dont have any voltage limits set and also dont know what p i and d values should be
+        hoodCfg.Slot0.kP = 10;
+        hoodCfg.Slot0.kI = 0;
+        hoodCfg.Slot0.kD = 0; 
+
         control.getConfigurator().apply(controlCfg);
+        hood.getConfigurator().apply(hoodCfg);
         
         TalonFXConfiguration followerCfg = new TalonFXConfiguration();
         followerCfg.MotorOutput.NeutralMode = NeutralModeValue.Brake;
@@ -58,6 +66,7 @@ public class ShooterSubsystem extends SubsystemBase {
         follower.setControl(new Follower(control.getDeviceID(), MotorAlignmentValue.Opposed));
 
         control.setNeutralMode(NeutralModeValue.Brake);
+        hood.setNeutralMode(NeutralModeValue.Brake); 
     }
 
      /**
@@ -84,6 +93,25 @@ public class ShooterSubsystem extends SubsystemBase {
         setTargetRpm(targetRpm + deltaRpm);
     }
 
+    
+    private void hoodStart() { // should run on startup, idk if necessary
+      hood.setPosition(0);
+    }
+
+    private void incrementHood(double rotations){
+      motor.set(ControlMode.Position, rotations);
+    }
+    /**
+     * @param target in rotations
+     */
+    private void setHoodPos(double target){
+      double currentPos = hood.getPosition().getValue();
+      double targetPos = currentPos + target;
+      while (currentPos < targetPos){
+        incrementHood(0.01); // adjust for overshoot if necessary
+        currentPos += 0.01; 
+      }
+    }
     public Command stop() {
       return new InstantCommand(() -> stopMotor());
     }
@@ -94,6 +122,18 @@ public class ShooterSubsystem extends SubsystemBase {
 
     public Command changeTargetRPM(double deltaRpm) {
       return new InstantCommand(() -> changeTargetRpm(deltaRpm));
+    }
+
+    public Command hoodUp(double rotations){
+      return new InstantCommand(() -> incrementHood(0.1));
+    }
+
+    public Command hoodDown(double rotations){
+      return new InstantCommand(() -> incrementHood(-0.1));
+    }
+
+    public Command setHood(double pos){
+      return new Instantcommand(() -> setHoodPos(pos));
     }
 
     public double getTargetRpm() {
